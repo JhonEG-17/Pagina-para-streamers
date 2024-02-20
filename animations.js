c570dd0bd1565f320f0d8f45806f1685efc7e484
JhonEG-17 <jhonataneg@outlook.com>
2024-02-20 00:18:08 -0600

    arregle el problema de encimacion de los elementos de la pagina y se agrego funcion de despliegue de informacion del carrito al dar click sobre el icono

--- animations.js
+++ animations.js
@@ -1,1 +1,17 @@
-animatios 
+document.addEventListener("DOMContentLoaded", function() {
+    const iconCart = document.querySelector('.icon-cart');
+    const cartProducts = document.querySelector('.container-cart-products');
+
+    // Agregar evento de clic al icono del carrito
+    iconCart.addEventListener('click', function() {
+        // Alternar la clase 'open' para mostrar u ocultar el carrito
+        cartProducts.classList.toggle('open');
+    });
+
+    // Agregar evento de clic al botón de cerrar en el carrito
+    const iconClose = document.querySelector('.icon-close');
+    iconClose.addEventListener('click', function() {
+        // Ocultar el carrito al hacer clic en el botón de cerrar
+        cartProducts.classList.remove('open');
+    });
+});
