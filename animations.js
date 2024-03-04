a1f029d89d98139cd9e654e8ee89bdbc98a900ba
JhonEG-17 <jhonataneg@outlook.com>
2024-03-04 10:34:18 -0600

    MOdificacion  a archivos logout.html, styles.css, animations.js. Se comienza a trabajar en las funciones de registrar usuario e iniciar secion para realizar el cambio de un menu a otro para poder llenar dicho formulario segun sea el caso ademas de darle un acabado mas apropiado referente a el diseño

--- animations.js
+++ animations.js
@@ -137,3 +137,22 @@ document.addEventListener("DOMContentLoaded", function() {
         countProducts.innerText = totalOfProducts;
     };
 });
+
+
+
+
+//Pertenese a la funcion de login & register
+
+
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+})
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+})
